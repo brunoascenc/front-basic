dd2600f26828b0622380625d2a351d14eb9c1d14
brunoascenc <brunoascenc1@outlook.com>
2021-09-23 11:41:53 -0300

    fetching api

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Código que será executado quando o navegador carregar
 });
+
+async function getInstaFeed() {
+  try {
+    const response = await fetch(
+      ' https://us-central1-squid-apis.cloudfunctions.net/test-front-basic'
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getInstaFeed();
